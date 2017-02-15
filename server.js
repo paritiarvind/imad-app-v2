babd18fbe0f95fa9fd5486790885704a6efbb45f
paritiarvind <paritiarvind@gmail.com>
2017-02-16 00:40:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,9 +98,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res){
+    var articleName=req.params.articleName;
  res.send(createTemplate(article[articleName]));
 });
-var articleName=req.params.articleName;
+   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
