791624aa9d589b48be9e7409bd6011a657102b53
paritiarvind <paritiarvind@gmail.com>
2017-02-16 01:43:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text'
 );
 element.innerHTML='New value';
 //move the image
-var img=document.getElementById('img');
-img.onclick=function(){
+var img=document.getElementById('madi');
+madi.onclick=function(){
     img.style.marginLeft='100px';
 };
