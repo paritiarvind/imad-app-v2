f5a22ede14b4fe36d5743214e898b143a23a2ade
paritiarvind <paritiarvind@gmail.com>
2017-02-16 23:41:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,6 @@ button.onclick=function()
             }
         }
 };
-request.open('GET',"http://paritiarvind.imad.hasura-app.io/counter", true);
-request.send(null);
+        request.open('GET',"http://paritiarvind.imad.hasura-app.io/counter", true);
+        request.send(null);
 };
