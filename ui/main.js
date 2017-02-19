2c8fffa69190538395d52d718d98e461feffa5df
paritiarvind <paritiarvind@gmail.com>
2017-02-19 23:47:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,8 @@ button.onclick=function()
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
+Submit.onclick=function()
+{
     var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
