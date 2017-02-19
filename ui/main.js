d2f8b9d47f130357b8c0adb2130f043f8a4a2ec4
paritiarvind <paritiarvind@gmail.com>
2017-02-20 00:19:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,9 @@ button.onclick=function()
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-window.onload = function(){ 
-submit.onclick=function(){
+
+submit.onclick=function()
+{
     var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
@@ -33,6 +34,4 @@ submit.onclick=function(){
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
-    
-};
 };
