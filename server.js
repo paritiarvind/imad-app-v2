c23dc04d656fbf7784ddce277005b9220624a28f
paritiarvind <paritiarvind@gmail.com>
2017-02-22 16:12:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,18 +88,11 @@ function createTemplate (data) {
                     ;
         return htmlTemplate;
 }
-var counter=0;
-app.get('/counter', function(req,res) {
-    counter=counter+1;
-    res.send(counter.toString());
-});
-var names=[];
-app.get('/:submit-name', function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
+
+
+
     
-});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
