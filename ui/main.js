d678147c8f87f22b1c9287259faa32f77242633d
paritiarvind <paritiarvind@gmail.com>
2017-02-28 01:25:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
  function myFunction() {
     document.body.style.backgroundImage = "url('http://www.hdbloggers.net/wp-content/uploads/2016/11/Dark-Wallpaper-Background.jpg')";
     document.body.style.backgroundRepeat = "repeat-y"; 
+ }
