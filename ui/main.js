f240eabcf4adb1941fbf26439bf5681f8b16e41d
paritiarvind <paritiarvind@gmail.com>
2017-03-01 00:24:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,3 @@
     document.body.style.backgroundImage = "url('http://www.hdbloggers.net/wp-content/uploads/2016/11/Dark-Wallpaper-Background.jpg')";
     document.body.style.backgroundRepeat = "repeat-n"; 
      }
-     function w3_open() {
-    document.getElementById("mySidenav").style.width = "100%";
-    document.getElementById("mySidenav").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("mySidenav").style.display = "none";
-}
