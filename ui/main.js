4edc0a42f296b4a3a273ce19301d8b851abf468d
paritiarvind <paritiarvind@gmail.com>
2017-03-01 23:51:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,15 @@
     document.body.style.backgroundImage = "url('http://www.guoguiyan.com/data/out/210/68673984-walls-wallpapers.jpg')";
     document.body.style.backgroundRepeat = "repeat-n"; 
      }
- function w3_open() {document.getElementById("mySidenav").style.display = "block"; }
- function w3_close() {
-                      document.getElementById("mySidenav").style.display = "none";
-                    }
-
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidenav").style.width = "25%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
                    
