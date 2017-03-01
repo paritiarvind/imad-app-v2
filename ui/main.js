2bcdf4a1222fc2fecf69a412be5b3675bd0ae67a
paritiarvind <paritiarvind@gmail.com>
2017-03-02 00:57:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
  function init() {
    
-    document.body.style.backgroundImage = "url('http://www.guoguiyan.com/data/out/210/68673984-walls-wallpapers.jpg')";
+    document.body.style.backgroundImage = "url('http:http://wallpapercave.com/wp/UoQbFHi.jpg')";
     document.body.style.backgroundRepeat = "repeat-n"; 
      }
 function w3_open() {
