2bc33a97d7ff4c754ef22062490b84b9750329d1
paritiarvind <paritiarvind@gmail.com>
2017-03-01 23:43:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,10 @@ function createTemplate (data) {
                            ${title}
                         </title>
                         <meta name="viewport" content="width=device-width; initial-scale=1.0"/>
-                          <link href="/ui/style.css" rel="stylesheet" />
+                        //   <link href="/ui/style.css" rel="stylesheet" />
+                         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                         <link rel="stylesheet" type='text/css' href='https://cloud.imad.hasura.io/code/files/ui%2Fstyle.css'>
+                        
                     </head>
                     <body>
                         <div class="container">
