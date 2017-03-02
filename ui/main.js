a01dceb0c76c2114c6a2796971a39d09126c0628
paritiarvind <paritiarvind@gmail.com>
2017-03-02 23:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,36 @@
-function w3_open() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("mySidenav").style.width = "25%";
-  document.getElementById("mySidenav").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
-}
-function w3_close() {
-  document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("mySidenav").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
-}
+// function w3_open() {
+//   document.getElementById("main").style.marginLeft = "25%";
+//   document.getElementById("mySidenav").style.width = "25%";
+//   document.getElementById("mySidenav").style.display = "block";
+//   document.getElementById("openNav").style.display = 'none';
+// }
+// function w3_close() {
+//   document.getElementById("main").style.marginLeft = "0%";
+//   document.getElementById("mySidenav").style.display = "none";
+//   document.getElementById("openNav").style.display = "inline-block";
+// }
 
-$(document).ready(function ()
-{
-	$('#particles').particleground(
-	{
-		minSpeedX: 0.1,
-		maxSpeedX: 0.7,
-		minSpeedY: 0.1,
-		maxSpeedY: 0.7,
-		directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-		directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
-		density: 7000, // How many particles will be generated: one particle every n pixels
-		dotColor: 'red',
-		lineColor: 'blue',
-		particleRadius: 7, // Dot size
-		lineWidth: 1,
-		curvedLines: false,
-		proximity: 100, // How close two dots need to be before they join
-		parallax: true,
-		parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
-		onInit: function () {},
-		onDestroy: function () {}
-	});
-});
+// $(document).ready(function ()
+// {
+// 	$('#particles').particleground(
+// 	{
+// 		minSpeedX: 0.1,
+// 		maxSpeedX: 0.7,
+// 		minSpeedY: 0.1,
+// 		maxSpeedY: 0.7,
+// 		directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+// 		directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+// 		density: 7000, // How many particles will be generated: one particle every n pixels
+// 		dotColor: 'red',
+// 		lineColor: 'blue',
+// 		particleRadius: 7, // Dot size
+// 		lineWidth: 1,
+// 		curvedLines: false,
+// 		proximity: 100, // How close two dots need to be before they join
+// 		parallax: true,
+// 		parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+// 		onInit: function () {},
+// 		onDestroy: function () {}
+// 	});
+// });
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('6 p(){2.3("f").4.g="b%";2.3("9").4.m="b%";2.3("9").4.8="c";2.3("d").4.8=\'e\'}6 n(){2.3("f").4.g="0%";2.3("9").4.8="e";2.3("d").4.8="l-c"}$(2).q(6(){$(\'#r\').k({i:0.1,h:0.7,j:0.1,o:0.7,s:\'a\',G:\'a\',F:E,D:\'H\',I:\'J\',K:7,C:1,B:v,u:t,w:x,A:5,z:6(){},y:6(){}})});',47,47,'||document|getElementById|style||function||display|mySidenav|center|25|block|openNav|none|main|marginLeft|maxSpeedX|minSpeedX|minSpeedY|particleground|inline|width|w3_close|maxSpeedY|w3_open|ready|particles|directionX|100|proximity|false|parallax|true|onDestroy|onInit|parallaxMultiplier|curvedLines|lineWidth|dotColor|7000|density|directionY|red|lineColor|blue|particleRadius'.split('|'),0,{}))
