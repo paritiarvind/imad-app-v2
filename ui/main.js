8dfb716aa24cf2e175e6e84c8a4e43a31ed97d05
paritiarvind <paritiarvind@gmail.com>
2017-03-02 17:17:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,8 +30,8 @@ $(document).ready(function ()
 		directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
 		directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
 		density: 10000, // How many particles will be generated: one particle every n pixels
-		dotColor: 'red',
-		lineColor: 'green',
+		dotColor: 'white',
+		lineColor: 'blue',
 		particleRadius: 7, // Dot size
 		lineWidth: 1,
 		curvedLines: false,
