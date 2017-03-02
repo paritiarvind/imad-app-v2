88bda4fab2d8be5551e4eda35801799cd4f61272
paritiarvind <paritiarvind@gmail.com>
2017-03-03 00:06:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ function createTemplate (data) {
     var htmlTemplate=`
                     <html>
                     <head>
-                        <title>
+                        <title align='center'>
                            ${title}
                         </title>
                         <meta name="viewport" content="width=device-width; initial-scale=1.0"/>
