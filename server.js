b9c4d6c6b0748586f31fa00691e80f57e127d268
paritiarvind <paritiarvind@gmail.com>
2017-03-02 14:59:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,6 +126,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+app.get('/ui/jquery.particleground.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.particleground.js'));
+});
 
 
  
