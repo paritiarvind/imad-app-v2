53f22c53582a7905e4a0d5b0715c67679fd33876
paritiarvind <paritiarvind@gmail.com>
2017-03-03 00:23:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,11 @@ $(document).ready(function ()
 });
 
 function w3_open() {
-    document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
 }
+
 function w3_close() {
-    document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
 }
