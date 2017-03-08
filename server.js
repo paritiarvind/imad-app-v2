fcfa94eccffac14bb2176d34433c74e8d5d31312
paritiarvind <paritiarvind@gmail.com>
2017-03-08 20:38:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ function createTemplate (data) {
     var content = data.content;
     var htmlTemplate=`
 <html>
-    //   <div class="container" align="center">
+       <div class="container" align="center">
          <title>${title}
          </title>
-    //   </div>
+       </div>
       <meta name="viewport" content="width=device-width; initial-scale=1.0"/>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <link rel="stylesheet"  href="https://www.w3schools.com/lib/w3.css">
