f0e7c8690cda2faafa8bf4236b79d2df2f53cb08
paritiarvind <paritiarvind@gmail.com>
2017-03-08 15:28:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,10 +49,6 @@ var articles = {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </p>`
                          },
-                Article4:{
-                   title:'Article4',
-                    content: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d957.7689549823621!2d81.61469094662628!3d21.263205998738332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDE1JzQ3LjUiTiA4McKwMzYnNTUuMiJF!5e1!3m2!1sen!2sin!4v1488965064557" width="800" height="600" frameborder="0" style="border:0" allowfullscreen> </iframe>
-                         }    
                 };  
                 
 function createTemplate (data) {
